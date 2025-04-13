de75c0e4bbc8bf9e79ce7b6677589985e259810e
Barak Levinson <barrylev10@gmail.com>
2025-04-13 18:32:16 +0300

    add initial server with Maven configuration and Spring Boot setup

---
+++ server/src/test/java/org/levinson/photoetcher/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.levinson.photoetcher.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
